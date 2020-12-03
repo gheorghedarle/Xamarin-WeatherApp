279f5e73a3be500c28a1a41461d7dab41136d6ba
gheorghedarle <darlegheorghe@yahoo.ro>
2020-12-03 15:17:11 +0200

    feat(app): add font sizes

---
+++ WeatherApp/WeatherApp/WeatherApp/Styles/Fonts.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace WeatherApp.Styles
+{
+    [Preserve(AllMembers = true)]
+    public partial class Fonts : ResourceDictionary
+    {
+        public Fonts()
+        {
+            InitializeComponent();
+        }
+    }
+}
