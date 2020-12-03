d10cf2f31898c9f5b0fdfd009fcf58196987c27a
gheorghedarle <darlegheorghe@yahoo.ro>
2020-12-03 14:06:49 +0200

    feat(app): add colors resource

---
+++ WeatherApp/WeatherApp/WeatherApp/Styles/Colors.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace WeatherApp.Styles
+{
+    [Preserve(AllMembers = true)]
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
