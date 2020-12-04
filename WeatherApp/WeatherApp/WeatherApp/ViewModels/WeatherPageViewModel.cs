0d2f913a3b67deb34ec59e6fd53a667a5f38be82
gheorghedarle <darlegheorghe@yahoo.ro>
2020-12-04 18:13:21 +0200

    wip(weather-page): add first binding

--- WeatherApp/WeatherApp/WeatherApp/ViewModels/WeatherPageViewModel.cs
+++ WeatherApp/WeatherApp/WeatherApp/ViewModels/WeatherPageViewModel.cs
@@ -7,7 +7,17 @@ namespace WeatherApp.ViewModels
 {
     public class WeatherPageViewModel : BaseViewModel
     {
+        public string CurrentDate { get; set; }
+
         public WeatherPageViewModel(INavigationService navigationService): base(navigationService)
-        { }
+        {
+            CurrentDate = CreateDateString();
+        }
+
+        private string CreateDateString()
+        {
+            var currentDate = DateTime.Now;
+            return currentDate.ToString("dddd, d MMMM yyyy");
+        }
     }
 }
