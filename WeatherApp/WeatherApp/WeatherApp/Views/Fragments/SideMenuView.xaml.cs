2c7759594b7645cbafaad42e2bd0a96988a86b82
gheorghedarle <darlegheorghe@yahoo.ro>
2021-01-12 19:31:45 +0200

    style(side-menu): add new UI for side menu

---
+++ WeatherApp/WeatherApp/WeatherApp/Views/Fragments/SideMenuView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.Views.Fragments
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SideMenuView : StackLayout
+    {
+        public SideMenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
