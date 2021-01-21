73b11a18f68265c0f77b1848e247731d63d34e3a
gheorghedarle <darlegheorghe@yahoo.ro>
2021-01-21 10:26:14 +0200

    fix(add-new-location): add error handling

--- WeatherApp/WeatherApp/WeatherApp/ViewModels/Dialogs/AddLocationDialogViewModel.cs
+++ WeatherApp/WeatherApp/WeatherApp/ViewModels/Dialogs/AddLocationDialogViewModel.cs
@@ -1,32 +1,47 @@
 ﻿using Newtonsoft.Json;
 using Prism.Mvvm;
+using Prism.Navigation;
 using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
 using WeatherApp.Models;
 using WeatherApp.Services.Location;
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace WeatherApp.ViewModels.Dialogs
 {
-    public class AddLocationDialogViewModel : BindableBase, IDialogAware
+    public class AddLocationDialogViewModel : BaseViewModel, IDialogAware
     {
         private List<LocationModel> _locations;
         private readonly LocationService _locationService;
 
         public Command UseCurrentLocationCommand { get; set; }
+        public bool HasError { get; set; }
 
-        public AddLocationDialogViewModel(LocationService locationService)
+        public AddLocationDialogViewModel(
+            INavigationService navigationService,
+            LocationService locationService): base(navigationService)
         {
             _locationService = locationService;
 
             UseCurrentLocationCommand = new Command<string>(UseCurrentLocationCommandHandler);
+
+            MainState = LayoutState.Loading;
         }
 
         private async void UseCurrentLocationCommandHandler(string locationName)
         {
+            MainState = LayoutState.Loading;
+            HasError = false;
             Location location = await _locationService.GetLocation(locationName);
+            if(location == null)
+            {
+                HasError = true;
+                MainState = LayoutState.None;
+                return;
+            }
             Placemark placemark = await _locationService.GetCurrentLocationName(location.Latitude, location.Longitude);
             var loc = new LocationModel()
             {
@@ -38,6 +53,7 @@ namespace WeatherApp.ViewModels.Dialogs
             };
             _locations.Add(loc);
             await SecureStorage.SetAsync("locations", JsonConvert.SerializeObject(_locations));
+            MainState = LayoutState.None;
             RequestClose(null);
         }
 
@@ -46,14 +62,13 @@ namespace WeatherApp.ViewModels.Dialogs
         public bool CanCloseDialog() => true;
 
         public void OnDialogClosed()
-        {
-
-        }
+        {}
 
         public async void OnDialogOpened(IDialogParameters parameters)
         {
             var listLocJson = await SecureStorage.GetAsync("locations");
             _locations = JsonConvert.DeserializeObject<List<LocationModel>>(listLocJson);
+            MainState = LayoutState.None;
         }
     }
 }
