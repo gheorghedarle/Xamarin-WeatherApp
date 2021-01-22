58c99ff223f3fd7ce76ab63bdef91c19f8f57ae3
gheorghedarle <darlegheorghe@yahoo.ro>
2021-01-22 12:38:09 +0200

    style(app): add styles file

---
+++ WeatherApp/WeatherApp/WeatherApp/Styles/Theme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Theme : ResourceDictionary
+    {
+        public Theme()
+        {
+            InitializeComponent();
+        }
+    }
+}
