59663c333394f2e5ea46c5ef3c8bb7179d7ee56c
gheorghedarle <darlegheorghe@yahoo.ro>
2021-02-02 17:44:39 +0200

    feat(android): add splash screen background

--- WeatherApp/WeatherApp/WeatherApp.Android/MainActivity.cs
+++ WeatherApp/WeatherApp/WeatherApp.Android/MainActivity.cs
@@ -9,11 +9,19 @@ using Android.OS;
 
 namespace WeatherApp.Droid
 {
-    [Activity(Label = "WeatherApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(
+        Label = "Weather App", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme.Splash", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            base.SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
